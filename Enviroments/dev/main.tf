8549f6984afafab066cb226735faf99602c7c7fc
kadamganesh26 <kadamganesh26@gmail.com>
2024-11-24 14:16:37 +0000

    added code upto validate

--- Enviroments/dev/main.tf
+++ Enviroments/dev/main.tf
@@ -1,16 +1,16 @@
 module "rg" {
-    source = "../../modules/azurerm_resource_group"
-    rg = var.dev-rg
+  source = "../../modules/azurerm_resource_group"
+  rg     = var.dev-rg
 }
 
 module "acr" {
-    depends_on = [ module.rg ]
-    source = "../../modules/azurerm-acr"
-    acr_clusters = var.dev-acr
+  depends_on   = [module.rg]
+  source       = "../../modules/azurerm-acr"
+  acr_clusters = var.dev-acr
 }
 
 module "aks" {
-depends_on = [ module.rg, module.acr]
-source = "../../modules/azurerm-aks"
-aks_clusters= var.dev-aks
+  depends_on   = [module.rg, module.acr]
+  source       = "../../modules/azurerm-aks"
+  aks_clusters = var.dev-aks
 }
