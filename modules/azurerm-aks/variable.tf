863de89d125eafc9fd94cfbf9332ea64964c4c3b
kadamganesh26 <kadamganesh26@gmail.com>
2024-11-24 06:08:32 +0000

    added AKS TF Code

---
+++ modules/azurerm-aks/variable.tf
@@ -0,0 +1,17 @@
+variable "var.aks_clusters" {
+  type = map()
+}
+
+variable "aks_clusters" {
+  description = "A map of AKS clusters, each containing details for the AKS resource group and the cluster itself"
+  type = map(object({
+    aks_rg_name        = string
+    aks_location       = string
+    aks_name           = string
+    resource_group_name = string
+    dns_prefix         = string
+    node_count         = number
+    vm_size            = string
+    tags               = string
+  }))
+}
